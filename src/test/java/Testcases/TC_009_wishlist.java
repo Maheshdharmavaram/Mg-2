f3149bc47871d27956f550c6d89d3c22b8508fb1
ADMIN <ADMIN@192.168.1.5>
2024-09-17 11:40:34 +0530

    commit Mogilix project

--- src/test/java/Testcases/TC_009_wishlist.java
+++ src/test/java/Testcases/TC_009_wishlist.java
@@ -8,6 +8,7 @@ import TestBase.BaseClass;
 public class TC_009_wishlist extends BaseClass {
 @Test
 	public void verifywishlist() throws InterruptedException {
+	//driver.get("https://www.moglix.com/");
         driver.get("https://www.moglix.com/epson-ecotank-17-ipm-a3-plus-wi-fi-duplex-wide-format-all-in-one-ink-tank-printer-l14150/mp/msn2km1jpm7x9v");
         wishlist wl = new wishlist(driver);
         wl.wishlist_click();	
